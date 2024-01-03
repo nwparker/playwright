ddf5b006669b8c7993a8460d423567e5d9fd11bb
Neil Parker <nwp7@cornell.edu>
2024-01-02 23:02:06 -0800

    Don't add network events (affects tl)

--- packages/trace-viewer/src/traceModel.ts
+++ packages/trace-viewer/src/traceModel.ts
@@ -242,10 +242,10 @@ export class TraceModel {
         this._snapshotStorage!.addFrameSnapshot(event.snapshot);
         break;
     }
-    if (event.type === 'action' || event.type === 'before')
-      contextEntry.startTime = Math.min(contextEntry.startTime, event.startTime);
-    if (event.type === 'action' || event.type === 'after')
-      contextEntry.endTime = Math.max(contextEntry.endTime, event.endTime);
+    // if (event.type === 'action' || event.type === 'before')
+    //   contextEntry.startTime = Math.min(contextEntry.startTime, event.startTime);
+    // if (event.type === 'action' || event.type === 'after')
+    //   contextEntry.endTime = Math.max(contextEntry.endTime, event.endTime);
     if (event.type === 'event') {
       contextEntry.startTime = Math.min(contextEntry.startTime, event.time);
       contextEntry.endTime = Math.max(contextEntry.endTime, event.time);
